5250e0d4703aaac941d2b93794ade2a590da0b24
MrFiona <1160177283@qq.com>
2017-11-08 20:15:47 +0800

    界面去掉多余的菜单

--- machine_scripts/wxpython_gui.py
+++ machine_scripts/wxpython_gui.py
@@ -221,7 +221,7 @@ def week_gui_config(purl_bak_string, logger):
 #todo 项目配置主界面
 class ProjectConfigParameterGui(wx.Frame):
     def __init__(self, name, logger):
-        wx.Frame.__init__(self, None, wx.ID_ANY,"%s User Configuration Gui Interface" % name, size=(930,715))
+        wx.Frame.__init__(self, None, wx.ID_ANY,"%s User Configuration Gui Interface" % name, size=(930,700))
         self.name = name
         self.logger = logger
 
@@ -391,18 +391,18 @@ class ProjectConfigParameterGui(wx.Frame):
         self.send_email_flag_text.Bind(wx.EVT_TEXT, self.send_email_text_frame_event)
         self.excel_template_button.Bind(wx.EVT_BUTTON, self.choose_excel_file_frame_event)
 
-        def onSelect(event):
-            print("onSelect")
-
-        menuBar = wx.MenuBar()
-        menu = wx.Menu()
-
-        menu.Append(-1, u'撤消(U)')
-        menu.AppendSeparator()
-        self.menu_select = menu.Append(-1, u'全选(N)\tCtrl+A')
-        self.Bind(wx.EVT_MENU, onSelect, self.menu_select)
-        menuBar.Append(menu, u'编辑(F)')
-        self.SetMenuBar(menuBar)
+        # def onSelect(event):
+        #     print("onSelect")
+        #
+        # menuBar = wx.MenuBar()
+        # menu = wx.Menu()
+        #
+        # menu.Append(-1, u'撤消(U)')
+        # menu.AppendSeparator()
+        # self.menu_select = menu.Append(-1, u'全选(N)\tCtrl+A')
+        # self.Bind(wx.EVT_MENU, onSelect, self.menu_select)
+        # menuBar.Append(menu, u'编辑(F)')
+        # self.SetMenuBar(menuBar)
 
     #todo 选择Excel模板文件按钮组件事件响应函数
     def choose_excel_file_frame_event(self, event):
